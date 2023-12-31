ad24460318168b0e39f0264f2f87b4a6ad471413
Masato Ikeda <masato.ikeda@gmail.com>
2024-01-01 01:21:52 +0900

    Implement AccessToken.issue

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -1,3 +1,11 @@
 class AccessToken < ApplicationRecord
   belongs_to :session
+
+  def self.issue(session)
+    begin
+      return AccessToken.create(session: session, token: SecureRandom.alphanumeric(32))
+    rescue ActiveRecord::RecordNotUnique
+      retry
+    end
+  end
 end
--- test/models/access_token_test.rb
+++ test/models/access_token_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class AccessTokenTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "issue returns an instance of AccessToken" do
+    user = User.create(name: "testuser", password: "testpassword", password_confirmation: "testpassword")
+    session = Session.create(user: user)
+    access_token = AccessToken.issue(session)
+    assert_equal AccessToken, access_token.class
+    assert_equal session, access_token.session
+    assert_equal String, access_token.token.class
+    assert_equal 32, access_token.token.length
+    assert_equal true, access_token.token.match?(/\A[a-zA-Z0-9]+\z/)
+  end
 end
