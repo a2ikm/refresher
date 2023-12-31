cd1c0eaee22ac38da62f89f5352fdc6aacb3169f
Masato Ikeda <masato.ikeda@gmail.com>
2024-01-01 01:28:20 +0900

    Implement Session.start

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,24 @@
 class Session < ApplicationRecord
   belongs_to :user
+
+  Result = Data.define(:name, :access_token)
+
+  def self.start(name, password)
+    user = User.authenticate(name, password)
+    if user.nil?
+      raise "Invalid name or password"
+    end
+
+    session = Session.create(user: user)
+    if session.nil?
+      raise "Could not create session"
+    end
+
+    access_token = AccessToken.issue(session)
+    if access_token.nil?
+      raise "Could not create access token"
+    end
+
+    return Result.new(name: user.name, access_token: access_token.token)
+  end
 end
--- test/models/session_test.rb
+++ test/models/session_test.rb
@@ -1,7 +1,41 @@
 require "test_helper"
 
 class SessionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "start with wrong name raises an error" do
+    user = User.create(name: "testuser", password: "testpassword", password_confirmation: "testpassword")
+    sessions_count = Session.count
+    access_token_count = AccessToken.count
+
+    assert_raises RuntimeError do
+      Session.start("wrongname", "testpassword")
+    end
+    assert_equal sessions_count, Session.count
+    assert_equal access_token_count, AccessToken.count
+  end
+
+  test "start with wrong password raises an error" do
+    user = User.create(name: "testuser", password: "testpassword", password_confirmation: "testpassword")
+    sessions_count = Session.count
+    access_token_count = AccessToken.count
+
+    assert_raises RuntimeError do
+      Session.start("testuser", "wronpassword")
+    end
+    assert_equal sessions_count, Session.count
+    assert_equal access_token_count, AccessToken.count
+  end
+
+  test "start with correct name and password returns an instance of Session::Result" do
+    user = User.create(name: "testuser", password: "testpassword", password_confirmation: "testpassword")
+    sessions_count = Session.count
+    access_token_count = AccessToken.count
+
+    result = Session.start("testuser", "testpassword")
+    assert_equal sessions_count + 1, Session.count
+    assert_equal access_token_count + 1, AccessToken.count
+
+    assert_equal Session::Result, result.class
+    assert_equal "testuser", result.name
+    assert_equal AccessToken.last.token, result.access_token
+  end
 end
