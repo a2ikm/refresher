c47f05142adc9fa72f065c6e21bc8f93fbd2144e
Masato Ikeda <masato.ikeda@gmail.com>
2024-01-01 01:43:55 +0900

    Implement POST /sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,11 @@
 class SessionsController < ApplicationController
+  skip_forgery_protection
+
   def create
+    result = Session.start(params[:name], params[:password])
+    render json: result.to_h
+  rescue RuntimeError => e
+    # TODO: error code should be varied for error kinds of client errors and server errors
+    render status: 500, json: { error: e.message }
   end
 end
