0ada3ad5f98550476c3822396c6aac9aeab4b8d6
Masato Ikeda <masato.ikeda@gmail.com>
2024-01-01 01:07:34 +0900

    Implement User.authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :sessions, dependent: :destroy
+
+  def self.authenticate(name, password)
+    user = User.find_by(name: name)
+    if user.nil? || !user.authenticate(password)
+      nil
+    else
+      user
+    end
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "authenticate with wrong name returns nil" do
+    User.create(name: "testuser", password: "testpassword", password_confirmation: "testpassword")
+    assert_nil User.authenticate("wrongname", "testpassword")
+  end
+
+  test "authenticate with wrong password returns nil" do
+    User.create(name: "testuser", password: "testpassword", password_confirmation: "testpassword")
+    assert_nil User.authenticate("testuser", "wrongpassword")
+  end
+
+  test "authenticate with correct name and password returns an instance of User" do
+    User.create(name: "testuser", password: "testpassword", password_confirmation: "testpassword")
+    user = User.authenticate("testuser", "testpassword")
+    assert_equal User, user.class
+    assert_equal "testuser", user.name
+  end
 end
