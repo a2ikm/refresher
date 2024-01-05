add52f095cd0d169d70fa96591959b63f1d49c1f
Masato Ikeda <masato.ikeda@gmail.com>
2024-01-05 22:01:12 +0900

    Implement RefreshToken like AccessToken

--- app/models/refresh_token.rb
+++ app/models/refresh_token.rb
@@ -1,3 +1,38 @@
 class RefreshToken < ApplicationRecord
+  EXPIRES_IN_SECONDS = 1.day
+
   belongs_to :session
+
+  def self.issue(session)
+    begin
+      now = Time.zone.now
+
+      return RefreshToken.create(
+        session: session,
+        token: SecureRandom.alphanumeric(32),
+        created_at: now,
+        updated_at: now,
+        expired_at: now + EXPIRES_IN_SECONDS,
+      )
+    rescue ActiveRecord::RecordNotUnique
+      retry
+    end
+  end
+
+  def self.verify(token)
+    refresh_token = RefreshToken.find_by(token: token)
+    if refresh_token.nil?
+      return nil
+    end
+
+    if refresh_token.expired?
+      return nil
+    end
+
+    refresh_token
+  end
+
+  def expired?
+    expired_at <= Time.zone.now
+  end
 end
--- test/models/refresh_token_test.rb
+++ test/models/refresh_token_test.rb
@@ -1,7 +1,63 @@
 require "test_helper"
 
 class RefreshTokenTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "issue returns an instance of RefreshToken" do
+    user = User.create(name: "testuser", password: "testpassword", password_confirmation: "testpassword")
+    session = Session.create(user: user)
+
+    refresh_token = RefreshToken.issue(session)
+
+    assert_equal RefreshToken, refresh_token.class
+    assert_equal session, refresh_token.session
+    assert_equal String, refresh_token.token.class
+    assert_equal 32, refresh_token.token.length
+    assert_equal true, refresh_token.token.match?(/\A[a-zA-Z0-9]+\z/)
+    assert_equal refresh_token.created_at + 1.day, refresh_token.expired_at
+  end
+
+  test "verify returns nil for unknown token" do
+    assert_nil RefreshToken.verify("unknowntoken")
+  end
+
+  test "verify returns nil for expired token" do
+    user = User.create(name: "testuser", password: "testpassword", password_confirmation: "testpassword")
+    session = Session.create(user: user)
+
+    refresh_token = RefreshToken.issue(session)
+
+    travel_to(refresh_token.expired_at, with_usec: true) do
+      assert_nil RefreshToken.verify(refresh_token.token)
+    end
+  end
+
+  test "verify returns an instance of RefreshToken for valid token" do
+    user = User.create(name: "testuser", password: "testpassword", password_confirmation: "testpassword")
+    session = Session.create(user: user)
+
+    refresh_token = RefreshToken.issue(session)
+    refresh_token.reload
+
+    travel_to(refresh_token.expired_at - 1.second, with_usec: true) do
+      assert_equal refresh_token, RefreshToken.verify(refresh_token.token)
+    end
+  end
+
+  test "expired? returns if RefreshToken is expired or now" do
+    user = User.create(name: "testuser", password: "testpassword", password_confirmation: "testpassword")
+    session = Session.create(user: user)
+
+    refresh_token = RefreshToken.issue(session)
+
+    travel_to(refresh_token.expired_at - 1.second, with_usec: true) do
+      assert_not refresh_token.expired?
+    end
+
+    travel_to(refresh_token.expired_at, with_usec: true) do
+      assert refresh_token.expired?
+    end
+
+    travel_to(refresh_token.expired_at + 1.second, with_usec: true) do
+      assert refresh_token.expired?
+    end
+  end
 end
