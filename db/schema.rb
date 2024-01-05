c6cd371067fb843862e560366d91d86d0b14ed1c
Masato Ikeda <masato.ikeda@gmail.com>
2024-01-05 21:44:07 +0900

    $ bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_01_114011) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_05_124113) do
   create_table "access_tokens", force: :cascade do |t|
     t.integer "session_id", null: false
     t.string "token", null: false
@@ -19,6 +19,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_01_114011) do
     t.datetime "expired_at", null: false
     t.index ["session_id"], name: "index_access_tokens_on_session_id"
     t.index ["token"], name: "index_access_tokens_on_token", unique: true
+  end
+
+  create_table "refresh_tokens", force: :cascade do |t|
+    t.integer "session_id", null: false
+    t.string "token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_refresh_tokens_on_session_id"
+    t.index ["token"], name: "index_refresh_tokens_on_token", unique: true
   end
 
   create_table "sessions", force: :cascade do |t|
@@ -37,5 +46,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_01_114011) do
   end
 
   add_foreign_key "access_tokens", "sessions"
+  add_foreign_key "refresh_tokens", "sessions"
   add_foreign_key "sessions", "users"
 end
