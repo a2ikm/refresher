c153dcfa15509bcce5c24e3cd69ad435781a73fa
Masato Ikeda <masato.ikeda@gmail.com>
2024-01-05 22:11:00 +0900

    Issue refresh token while running StartSession

--- app/models/commands/start_session.rb
+++ app/models/commands/start_session.rb
@@ -1,12 +1,13 @@
 module Commands::StartSession
-  Result = Data.define(:name, :access_token)
+  Result = Data.define(:name, :access_token, :refresh_token)
 
   # @raise [Commands::Authenticate::Failed] raised if authentication failed
   def self.run(name, password)
     user = Commands::Authenticate.run(name, password)
     session = Session.create!(user: user)
     access_token = AccessToken.issue(session)
+    refresh_token = RefreshToken.issue(session)
 
-    return Result.new(name: user.name, access_token: access_token.token)
+    return Result.new(name: user.name, access_token: access_token.token, refresh_token: refresh_token.token)
   end
 end
--- test/models/commands/start_session_test.rb
+++ test/models/commands/start_session_test.rb
@@ -5,37 +5,44 @@ class CommandsStartSessionTest < ActiveSupport::TestCase
     user = User.create(name: "testuser", password: "testpassword", password_confirmation: "testpassword")
     sessions_count = Session.count
     access_token_count = AccessToken.count
+    refresh_token_count = RefreshToken.count
 
     assert_raises Commands::Authenticate::Failed do
       Commands::StartSession.run("wrongname", "testpassword")
     end
     assert_equal sessions_count, Session.count
     assert_equal access_token_count, AccessToken.count
+    assert_equal refresh_token_count, RefreshToken.count
   end
 
   test "raises Commands::Authenticate::Failed for wrong password" do
     user = User.create(name: "testuser", password: "testpassword", password_confirmation: "testpassword")
     sessions_count = Session.count
     access_token_count = AccessToken.count
+    refresh_token_count = RefreshToken.count
 
     assert_raises Commands::Authenticate::Failed do
       Commands::StartSession.run("testuser", "wronpassword")
     end
     assert_equal sessions_count, Session.count
     assert_equal access_token_count, AccessToken.count
+    assert_equal refresh_token_count, RefreshToken.count
   end
 
   test "returns an instance of Commands::StartSession::Result for correct name and password" do
     user = User.create(name: "testuser", password: "testpassword", password_confirmation: "testpassword")
     sessions_count = Session.count
     access_token_count = AccessToken.count
+    refresh_token_count = RefreshToken.count
 
     result = Commands::StartSession.run("testuser", "testpassword")
     assert_equal sessions_count + 1, Session.count
     assert_equal access_token_count + 1, AccessToken.count
+    assert_equal refresh_token_count + 1, RefreshToken.count
 
     assert_equal Commands::StartSession::Result, result.class
     assert_equal "testuser", result.name
     assert_equal AccessToken.last.token, result.access_token
+    assert_equal RefreshToken.last.token, result.refresh_token
   end
 end
