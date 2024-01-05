819502024c99b078cf19f66b06d9e834f42c2fcf
Masato Ikeda <masato.ikeda@gmail.com>
2024-01-05 21:43:46 +0900

    Tweak not null constraint

--- db/migrate/20240105124113_create_refresh_tokens.rb
+++ db/migrate/20240105124113_create_refresh_tokens.rb
@@ -2,7 +2,7 @@ class CreateRefreshTokens < ActiveRecord::Migration[7.1]
   def change
     create_table :refresh_tokens do |t|
       t.references :session, null: false, foreign_key: true
-      t.string :token
+      t.string :token, null: false
 
       t.timestamps
     end
