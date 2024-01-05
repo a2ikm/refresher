88f782c79fcab079e98fcca6302a502bcd7e2381
Masato Ikeda <masato.ikeda@gmail.com>
2024-01-05 18:30:11 +0900

    Add tests for POST /sessions

--- test/integration/create_session_test.rb
+++ test/integration/create_session_test.rb
@@ -10,4 +10,25 @@ class CreateSessionTest < ActionDispatch::IntegrationTest
     post "/sessions", params: { name: "testuser", password: nil }
     assert_response :bad_request
   end
+
+  test "respond 401 if name parameter is not invalid" do
+    User.create(name: "testuser", password: "testpassword", password_confirmation: "testpassword")
+
+    post "/sessions", params: { name: "wronguser", password: "testpassword" }
+    assert_response :unauthorized
+  end
+
+  test "respond 401 if password parameter is not invalid" do
+    User.create(name: "testuser", password: "testpassword", password_confirmation: "testpassword")
+
+    post "/sessions", params: { name: "testuser", password: "wrongpassword" }
+    assert_response :unauthorized
+  end
+
+  test "respond 200 if name and password are correct" do
+    User.create(name: "testuser", password: "testpassword", password_confirmation: "testpassword")
+
+    post "/sessions", params: { name: "testuser", password: "testpassword" }
+    assert_response :ok
+  end
 end
